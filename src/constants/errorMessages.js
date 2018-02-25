3caea854b1d2012d410b55b075ea0d7fdb75337a
szymonsitko <szymon.sitko@onet.eu>
2018-02-25 22:21:45 +0100

    Fixed wrong error message reference

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,7 +1,7 @@
 const NOT_PROVIDED_OR_INVALID = "Browser element is not provided or its type is invalid. Please make sure that the instance of WebdriverIO is configured.";
-const INVALID_BROWSER_OBJECT = "Browser object exists, but appears to be invalid. Please double check your WebdriverIO confiuration.";
+const INVALID_BROWSER_OBJECT = "Browser object exists, but appears to be invalid. Please double check your WebdriverIO configuration.";
 const UNDEFINED_GETHTML_FUNCTION = "Valid browser object should contain native 'getHTML' method, got undefined instead.";
-const MISSING_CONSTRUCTOR = "BindDocument function was invoked with wrong parameter: DOMBuilder is missing.";
+const MISSING_CONSTRUCTOR = "BindDocument function was invoked with wrong parameter: DOMBuilder is not present.";
 
 module.exports = {
     error: {
